0fdd70aef1cca5049331ab850d1841bbd253dba8
KennOtieno <otiken001@gmail.com>
2023-11-09 09:29:01 +0300

    Main Activity file.......

--- MainActivity.kt
+++ MainActivity.kt
@@ -9,10 +9,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Decare variables as arrays so as to group together the Number and Expressions
-        var myNumbers: Int = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
-        var myExpressions: String = arrayOf(+, -, /, *, (, ), =)
+        var myNumbers = arrayOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+        var myExpressions = arrayOf("+", "-", "*", "(", ")", "=", "/")
 
         // All the variables above are Buttons
+        myNumbers: Button
+        myExpressions: Button
+
         myNumbersButton = findViewById<Button>(R.id.myNumbersButton)
         myExpressionsButton = findViewById<Button>(R.id.myExpressionsButton)
     }
